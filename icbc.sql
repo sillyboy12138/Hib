a8087634e610c9a6575de810837d13e226ce2b5a
sillyboy12138 <92001470+sillyboy12138@users.noreply.github.com>
2021-10-14 17:14:08 +0800

    Add files via upload

---
+++ icbc.sql
@@ -0,0 +1,8 @@
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('12345678', 'test', '123456', '1', '1', '1', '1', 911139204, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('22222223', 'ghb', '123456', '1', '1', '1', '1', 10000, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('26290140', '1', '2', '1', '1', '1', '22', 2000, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('27183478', '·', '1', '2', '2', '2', '2', 222, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('46167776', '1', '12', '22', '222', '111', '2222', 11111, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('50530374', '222', '23', '2', '11', '222', '1', 1111, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('70527370', '1', '1', '1', '1', '1', '1', 1, '中国工商银行昌平支行');
+INSERT INTO `icbc` (`username`, `namee`, `password`, `country`, `province`, `street`, `gate`, `moneyy`, `bankname`) VALUES ('87654321', 'test', '654321', '2', '2', '2', '2', 656321, '中国工商银行昌平支行');
