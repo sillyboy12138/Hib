6ab92be02dcfa9a9163b29cb4b5010e0f125c1d3
sillyboy12138 <92001470+sillyboy12138@users.noreply.github.com>
2021-11-08 14:18:13 +0800

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -13,22 +13,18 @@ to='3072791869@qq.com'
 t=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
 subject='python repetion test' +t
 
-fp=open("E:\sublime\python\day13\计算器.html","r"，encoding="utf-8")
-msg=email.message_from_file(fp)
-fp.close()
-for par in msg.walk():
-    if not par.is_multipart():
-        connect=par.get_payload(decode=True).decode('utf-8')
-
 def send_email():
-    msg=MIMEText(connect,'html')
+    msg=MIMEMultipart()
+    part_text=MIMEText('来自郭宏博的运算测试报告')
+    msg.attach(part_text)
     msg["Subject"]=Header(subject,'utf-8')
     msg["From"]=msg_from
     msg["To"]=to
-
+    connect = MIMEApplication(open(r'E:\sublime\python\day13\计算器.html', 'rb').read())
+    connect.add_header('Content-Disposition', 'attachment', filename='运算测试')
+    msg.attach(connect)
     #发送邮件            服务器地址
     ss=smtplib.SMTP_SSL("smtp.qq.com")
-    ss.connect("smtp.qq.com")
     ss.login(msg_from,pwd)
     #            发送方
     ss.sendmail(msg_from,to,msg.as_string())
