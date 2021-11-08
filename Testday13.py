2243f0757ae22bd3754445f32c6740239d95ff48
sillyboy12138 <92001470+sillyboy12138@users.noreply.github.com>
2021-11-08 10:29:53 +0800

    Update Testday13.py

--- Testday13.py
+++ Testday13.py
@@ -5,7 +5,7 @@ from ddt import ddt,data,unpack
 @ddt
 class MyTestCase(unittest.TestCase):
     def steUp(self) ->None:
-        r = Repetion()
+        
         time.sleep(0.01)
 
     def tearDown(self) -> None:
@@ -14,25 +14,25 @@ class MyTestCase(unittest.TestCase):
     @data([5,6,11],[-5,6,1],[5,-6,-1],[-5,-6,-10])
     @unpack
     def testadd(self,num1,num2,s):
-
+        r = Repetion()
         sum=r.TestAdd(num1,num2)
         self.assertEqual(sum, s)
     @data([5,6,-1],[-5,6,-11],[5,-6,11],[-5,-6,1])
     @unpack
     def testsubtract(self,num1,num2,s):
-        # r = Repetion()
+        r = Repetion()
         sum=r.TestSubtract(num1,num2)
         self.assertEqual(sum, s)
     @data([5,6,30],[-5,6,-30],[5,-6,-30],[-5,-6,3])
     @unpack
     def testmultiply(self,num1,num2,s):
-        # r = Repetion()
+        r = Repetion()
         sum=r.TestMultiply(num1,num2)
         self.assertEqual(sum, s)
     @data([10,5,2],[-10,5,-2],[10,-5,-2],[-10,-5,20])
     @unpack
     def testdivide(self,num1,num2,s):
-        # r = Repetion()
+        r = Repetion()
         sum=r.TestDivide(num1,num2)
         self.assertEqual(sum, s)
 
