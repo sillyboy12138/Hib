f6d886e6ff8ec8f6c489555df21cb8f2805237de
sillyboy12138 <92001470+sillyboy12138@users.noreply.github.com>
2021-11-10 15:19:04 +0800

    Update day14.py

--- day14.py
+++ day14.py
@@ -1,7 +1,9 @@
 import unittest
 from 工商银行面向对象 import Bank,chaxun
-from Read_excel import get_data,write1_data,write2_data
+from Read_excel import get_data
 import xlwt
+from xlutils import copy
+import xlrd
 from ddt import ddt,data,unpack
 b=Bank()
 aa=get_data(r'C:\Users\呆瓜\Desktop\icbc.xlsx',0)
@@ -14,14 +16,31 @@ class MyTestCase(unittest.TestCase):
     def test_transfer(self, Id, save_password, change_user, change_money, retu,row):
         ret=b.transfer(Id,save_password,change_user,change_money)
         self.assertEqual(ret,retu)
-        write1_data(ret,retu,row)
+        wb = xlrd.open_workbook(r'C:\Users\呆瓜\Desktop\icbc.xlsx')
+        wb = copy.copy(wb)
+        worksheet = wb.get_sheet(0)
+        if ret == retu:
+            worksheet.write(row, 8, 'pass')
+            wb.save('icbc.xlsx')
+        else:
+            worksheet.write(row, 8, 'false')
+            wb.save('icbc.xlsx')
 
     @data(*bb)
     @unpack
     def test_select(self, Id, save_password, retu,row):
         ret=chaxun(Id,save_password)
         self.assertEqual(ret,retu)
-        write2_data(ret,retu,row)
+        wb = xlrd.open_workbook(r'C:\Users\呆瓜\Desktop\icbc.xlsx')
+        wb = copy.copy(wb)
+        worksheet = wb.get_sheet(1)
+        if ret == retu:
+            worksheet.write(row, 6, 'pass')
+            wb.save('icbc.xlsx')
+        else:
+            worksheet.write(row, 6, 'false')
+            wb.save('icbc.xlsx')
+
 
 if __name__ == '__main__':
     unittest.main()
