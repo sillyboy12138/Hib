ecdef7d88e5b37bcddcf8422a5bd0107ecb85f24
sillyboy12138 <92001470+sillyboy12138@users.noreply.github.com>
2021-11-10 15:17:49 +0800

    Update Read_excel.py

--- Read_excel.py
+++ Read_excel.py
@@ -1,5 +1,4 @@
 import copy
-
 import xlrd
 import xlwt
 
@@ -13,23 +12,3 @@ def get_data(file_name,index):
     for i in range(1, sheet.nrows):
         aa.append(list(sheet.row_values(i, 1, sheet.ncols-1)))
     return aa
-
-def write1_data(ret,retu,row):
-    workbook=copy.copy(table1)
-    worksheet=workbook.get_sheet(0)
-    if ret==retu:
-        worksheet.write(row,7,'pass')
-        workbook.save('icbc.xlsx')
-    else:
-        worksheet.write(row,7,'false')
-        workbook.save('icbc.xlsx')
-
-def write2_data(ret,retu,row):
-    workbook = copy.copy(table2)
-    worksheet = workbook.get_sheet(1)
-    if ret == retu:
-        worksheet.write(row, 6, 'pass')
-        workbook.save('icbc.xlsx')
-    else:
-        worksheet.write(row,6,'false')
-        workbook.save('icbc.xlsx')
